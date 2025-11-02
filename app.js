48f306882842199d91944628c2ddd3365b9ca737
Luis Flores Pareja <luis.flores@agrosoft.com.ec>
2025-11-01 23:55:51 -0500

    Test CI pipeline 7

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, Kubernetes from OCI\n');
+    res.end('Hello, Kubernetes ArgoCD from OCI!\n');
 });
 
 server.listen(PORT, () => {
